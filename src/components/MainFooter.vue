49e2de8bf5c15dbc4f56cd2f2217cdac9e4b235e
kizya007 <kizya007@mail.ru>
2023-07-27 13:33:20 +0200

    create footer and end work landing page

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,21 +1,83 @@
 <template>
-    <div>
-
+  <footer>
+    <div class="content">
+      <ul class="content-block">
+        <li class="content-block__img">
+          <img src="@/assets/png/efood.png" alt="" />
+        </li>
+        <li class="content-block__info">
+          <PointIcon />
+          Dhaka, Bangladesh
+        </li>
+        <li class="content-block__info">
+          <PhoneIcon />
+          0943833399
+        </li>
+        <li class="content-block__info">
+          <EmailSendIcon />
+          support@efood.com
+        </li>
+        <li class="content-block__image">
+          <img src="@/assets/png/Sosmed.png" alt="" />
+        </li>
+      </ul>
+      <ul class="right-block">
+        <li class="right-block__title">Service</li>
+        <li class="right-block__text">How it Works</li>
+        <li class="right-block__text">Home delivery</li>
+        <li class="right-block__text">Products</li>
+        <li class="right-block__text">Menu</li>
+      </ul>
+      <ul class="right-block">
+        <li class="right-block__title">Company</li>
+        <li class="right-block__text">About Us</li>
+        <li class="right-block__text">News</li>
+        <li class="right-block__text">Our trusted partner</li>
+        <li class="right-block__text">New users FAQ</li>
+      </ul>
+      <ul class="right-block">
+        <li class="right-block__title">Supports</li>
+        <li class="right-block__text">Help Center</li>
+        <li class="right-block__text">Feedback</li>
+        <li class="right-block__text">Contact us</li>
+        <li class="right-block__text">Terms conditions</li>
+      </ul>
+      <ul class="right-block">
+        <li class="right-block__title">Resources</li>
+        <li class="right-block__text">Download app</li>
+        <li class="right-block__text">Blog</li>
+        <li class="right-block__text">What's new</li>
+        <li class="right-block__text">Sitemap</li>
+      </ul>
     </div>
+    <div class="line"></div>
+    <div class="privacy">
+      <p class="privacy-info">© 2021 EFood - All rights reserved.</p>
+      <ul class="privacy-list">
+        <li class="privacy-list__item">Privacy</li>
+        <li class="privacy-list__item">Security</li>
+        <li class="privacy-list__item">Terms</li>
+      </ul>
+    </div>
+  </footer>
 </template>
 
 <script>
-
+import PointIcon from "@/assets/icon/PointIcon.vue";
+import PhoneIcon from "@/assets/icon/PhoneIcon.vue";
+import EmailSendIcon from "@/assets/icon/EmailSendIcon.vue";
 
 export default {
-    name: 'MainFooter',
+  name: "MainFooter",
 
-    components: {
-        
-    }
-}
+  components: {
+    PointIcon,
+    PhoneIcon,
+    EmailSendIcon,
+  },
+};
 </script>
 
-<style lang="scss" scoped >
-@import '@/assets/Footer.scss';
+<style lang="scss" scoped>
+@import "@/assets/Footer.scss";
 </style>
