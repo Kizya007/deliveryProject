d009daa070b0f8246810386a3635b5b4f0bcdcde
kizya007 <kizya007@mail.ru>
2023-09-06 21:04:59 +0200

    refactor header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,31 +1,33 @@
 <template>
-  <header class="container">
-    <p class="title">Efood</p>
-    <nav class="navigation">
-      <ul class="navigation-list">
-        <li class="navigation-list__item">
-          <a href=""> Home </a>
-        </li>
-        <li class="navigation-list__item">
-          <a href=""> Service </a>
-        </li>
-        <li class="navigation-list__item">
-          <a href=""> Top cities </a>
-        </li>
-        <li class="navigation-list__item">
-          <a href=""> Contact </a>
-        </li>
+  <header class="header">
+    <div class="container">
+      <p class="title">Efood</p>
+      <nav class="navigation">
+        <ul class="navigation-list">
+          <li class="navigation-list__item">
+            <a href=""> Home </a>
+          </li>
+          <li class="navigation-list__item">
+            <a href=""> Service </a>
+          </li>
+          <li class="navigation-list__item">
+            <a href=""> Top cities </a>
+          </li>
+          <li class="navigation-list__item">
+            <a href=""> Contact </a>
+          </li>
 
-        <li class="navigation-list__item">
-          <SearchIcon />
-        </li>
-        <li class="navigation-list__item">
-          <BasketIcon />
-          <span class="navigation-list__item-count">4</span>
-        </li>
-      </ul>
-    </nav>
-    <button class="btn" href="" v-on:click="handleClick">Sign Up</button>
+          <li class="navigation-list__item">
+            <SearchIcon />
+          </li>
+          <li class="navigation-list__item">
+            <BasketIcon />
+            <span class="navigation-list__item-count">4</span>
+          </li>
+        </ul>
+      </nav>
+      <button class="btn" href="" v-on:click="handleClick">Sign Up</button>
+    </div>
   </header>
 </template>
 
